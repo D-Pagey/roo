e2a48116150ff361b1bff2d53adaff2ac4b4708d
Dan Page <dan.page91@gmail.com>
2019-11-14 14:09:53 +0000

    build(styled components): add and config styled components and linting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/94a5d1ca-7c56-458c-8342-86f642940d30/deploy-status)](https://app.netlify.com/sites/pagey-roo/deploys)
+
+- add commitizen
+- run git cz to commit easily per config
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
+import * as S from './styles';
 
-const App = () => {
-    return (
-        <div>
-            <header>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
-};
+const App = () => (
+    <S.Wrapper>
+        <header>
+            <h1>Deliveroo Tech Test</h1>
+        </header>
+    </S.Wrapper>
+);
 
 export default App;
