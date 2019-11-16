d361c83746fac7fc00a16b1a01ba7ebaca82633f
Dan Page <dan.page91@gmail.com>
2019-11-16 21:32:03 +0000

    test(navbar): add tests for the media query

--- src/components/Navbar/index.test.js
+++ src/components/Navbar/index.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { useMediaQuery } from 'react-responsive';
 import Navbar from '.';
 
+jest.mock('react-responsive', () => ({
+    useMediaQuery: jest.fn(() => false)
+}));
+
 describe('Navbar component', () => {
-    it('should render', () => {
-        const { container } = render(<Navbar />);
+    it('should render with small logo', () => {
+        const { container, getByTestId } = render(<Navbar />);
         expect(container.firstChild).toMatchSnapshot();
+        getByTestId('navbarSmallLogo');
+    });
+
+    it('should render with large logo of non-mobile devices', () => {
+        useMediaQuery.mockImplementation(() => true);
+
+        const { getByTestId } = render(<Navbar />);
+        getByTestId('navbarLogo');
     });
 });
