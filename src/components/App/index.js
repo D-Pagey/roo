415118b8e14df3928bf3a0c33405a735130e1364
Dan Page <dan.page91@gmail.com>
2019-11-19 15:15:55 +0000

    feat: interview code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,16 +9,79 @@ import * as S from './styles';
 // mock data
 import data from '../../restaurants';
 
+// [{
+//     name: 'Mexican',
+//     count: 24
+// }]
+
+// {
+//     mexican : 24
+// }
+
+// get all the names
+// get the counts
+
+const getAllTags = (array) => {
+    return array.reduce((acc, curr) => {
+        return [...acc, ...curr.tags];
+    }, []);
+};
+
+const handleDuplicates = (array) => {
+    return array.reduce((acc, curr) => {
+        // check if exists in acc
+        if (acc[curr]) {
+            return {
+                ...acc,
+                [curr]: acc[curr] + 1
+            };
+        }
+
+        return {
+            ...acc,
+            [curr]: 1
+        };
+        // if yes, increase count
+        // if it doesnt, add with count of 1
+    }, {});
+};
+
+const cats = handleDuplicates(getAllTags(data.restaurants));
+const keys = Object.keys(cats);
+
+// take a cat
+// check if it exists in the tags with in rest
+// filter that just rest than include tag
+
+const filterData = (array, cat) => {
+    if (cat === '') return array;
+
+    return array.filter((item) => {
+        return item.tags.includes(cat);
+    });
+
+    // check tags.includes filter
+    // filter down
+};
+
 const App = () => {
     const [neighborhood, setNeighborhood] = useState('');
     const [restaurants, setRestaurants] = useState([]);
+    const [filter, setFilter] = useState('');
     const isTabletOrLarger = useMediaQuery({ query: mediaQuery.tablet });
 
     useEffect(() => {
-        // this is unneccessary for mock data however this is where I would fetch real data
+        setRestaurants(filterData(data.restaurants, filter));
+    }, [filter]);
+
+    useEffect(() => {
+        // state changes
+        // run this filter function
         setNeighborhood(data.neighborhood);
-        setRestaurants(data.restaurants);
+        // setRestaurants(data.restaurants);
     }, []);
+
+    const handleClick = (cat) => () => setFilter(cat);
 
     return (
         <S.Wrapper>
@@ -27,9 +90,15 @@ const App = () => {
             <Navbar />
             <Location location={neighborhood} />
 
-            {isTabletOrLarger && (
-                <S.Body data-testid="appQuantity">{restaurants.length} Restaurants</S.Body>
-            )}
+            <ul>
+                {keys.map((key) => {
+                    return (
+                        <li onClick={handleClick(key)}>
+                            {key} ({cats[key]})
+                        </li>
+                    );
+                })}
+            </ul>
 
             <RestaurantList restaurants={restaurants} />
         </S.Wrapper>
